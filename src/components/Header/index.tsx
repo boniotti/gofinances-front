6f1e0596e697f470fe1fe969dc7d3fc782b23473
Fernando Boniotti <fernando.boniotti@gmail.com>
2020-04-21 11:51:18 -0300

    Desafio: Fundamentos ReactJS

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
 
 import { Container } from './styles';
 
@@ -10,17 +10,27 @@ interface HeaderProps {
   size?: 'small' | 'large';
 }
 
-const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
-  <Container size={size}>
-    <header>
-      <img src={Logo} alt="GoFinances" />
-      <nav>
-        {
-          // Todo
-        }
-      </nav>
-    </header>
-  </Container>
-);
+interface HeaderParams {
+  header: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => {
+  const { path } = useRouteMatch();
+  return (
+    <Container size={size}>
+      <header>
+        <img src={Logo} alt="GoFinances" />
+        <nav>
+          <Link to="/" className={path === '/' ? 'active' : ''}>
+            Dashboard
+          </Link>
+          <Link to="/import" className={path === '/import' ? 'active' : ''}>
+            Importar
+          </Link>
+        </nav>
+      </header>
+    </Container>
+  );
+};
 
 export default Header;
--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,18 +23,26 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
+    const data = new FormData();
+    data.append('file', uploadedFiles[0].file);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.goBack();
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
+    setUploadedFiles([
+      {
+        file: files[0],
+        name: files[0].name,
+        readableSize: filesize(files[0].size),
+      },
+    ]);
+
     // TODO
   }
 
